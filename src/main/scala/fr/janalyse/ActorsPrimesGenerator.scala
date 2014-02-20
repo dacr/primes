03d2e275c5f9eec4dc0d1f899bc672a44b0f0ff0
crosson.david@gmail.com <crosson.david@gmail.com>
2014-02-20 22:18:34 +0100

    going on with actors based primes generator

--- src/main/scala/fr/janalyse/ActorsPrimesGenerator.scala
+++ src/main/scala/fr/janalyse/ActorsPrimesGenerator.scala
@@ -2,12 +2,14 @@ package fr.janalyse.primes
 
 import akka.actor._
 import ActorDSL._
+import akka.routing.SmallestMailboxRouter
+import scala.collection.immutable.Queue
 
 class ActorsPrimesGenerator[NUM](
-  name: String = "DefaultPrimesGenerator",
+  name: String = "DefaultPrimesGeneratorSystem",
   startFrom: NUM = 2,
-  primeNth: NUM,
-  notPrimeNth: NUM)(implicit numops: Integral[NUM]) extends PrimesDefinitions[NUM] {
+  primeNth: NUM = 1,
+  notPrimeNth: NUM = 0)(implicit numops: Integral[NUM]) extends PrimesDefinitions[NUM] {
   import numops._
 
   implicit val system = ActorSystem(name)
@@ -25,23 +27,50 @@ class ActorsPrimesGenerator[NUM](
   }
 
   case class PartialResult(value:NUM, isPrime:Boolean)
+  case class NextCheckedValue()
   
-  class ValueManagerActor extends Actor {
-    private var currentPrimeNth=primeNth
-    private var currentNotPrimeNth=notPrimeNth
-    private var buffer=List.empty[NUM]
-    private var nextValue=startFrom
+  class CheckerActor extends Actor {
     def receive = {
-      case PartialResult(value, true) if value == nextValue =>
-        currentPrimeNth+=one
-      case PartialResult(value, false) if value == nextValue =>
-        currentNotPrimeNth+=one
-      case pr:PartialResult => // Then delay
+      case NextValue(value) =>
+        manager ! PartialResult(value, isPrime(value))
     }
   }
   
   
-  private val next = system.actorOf(Props[NextValueActor], "NextValueActor")
-  private val manager =  system.actorOf(Props[ValueManagerActor], "ValueManagerActor")
+  class ValuesManagerActor extends Actor {
+    val vma = self
+    val checkerRouter = system.actorOf(
+        Props(classOf[CheckerActor], vma).withRouter(SmallestMailboxRouter(4)),
+        "CheckerActorRouter")
+    
+    private var currentPrimeNth=primeNth
+    private var currentNotPrimeNth=notPrimeNth
+    private var buffer=List.empty[NUM]
+    private var nextValue=startFrom
+    
+    private var checkedValuesQueue = Queue.empty[CheckedValue[NUM]]
+    
+    def receive = {
+      case PartialResult(value, true) if value == nextValue =>
+        currentPrimeNth+=one
+        val res = CheckedValue[NUM](value, true, value.toString.size, currentPrimeNth)
+        checkedValuesQueue = checkedValuesQueue.enqueue(res)
+        
+      case PartialResult(value, false) if value == nextValue =>
+        currentNotPrimeNth+=one
+        val res = CheckedValue[NUM](value, false, value.toString.size, currentNotPrimeNth)
+        checkedValuesQueue = checkedValuesQueue.enqueue(res)
+        
+      case NextCheckedValue() =>
+        //val next =
+        //checkedValuesQueue = checkedValuesQueue.enqueue(res)
+        
+      case pr:PartialResult => // Then delay
+        
+    }
+  }
+  
+  
+  private val manager =  system.actorOf(Props[ValuesManagerActor], "ValueManagerActor")
   
 }
