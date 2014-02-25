8df1ae841048fcdd33936f54735e81c04bf684c4
crosson.david@gmail.com <crosson.david@gmail.com>
2014-02-25 23:15:25 +0100

    YES - akka actors based primes generator is ready !!!

--- src/main/scala/fr/janalyse/ActorsPrimesGenerator.scala
+++ src/main/scala/fr/janalyse/ActorsPrimesGenerator.scala
@@ -6,10 +6,6 @@ import akka.routing.SmallestMailboxRouter
 import scala.collection.immutable.Queue
 
 
-object ActorsPrimesGenerator {
-}
-
-
 class ActorsPrimesGenerator[NUM](
   name: String = "DefaultPrimesGeneratorSystem",
   startFrom: NUM = 2,
@@ -22,10 +18,15 @@ class ActorsPrimesGenerator[NUM](
   case class NextValue(value: NUM)
   case class PartialResult(value:NUM, isPrime:Boolean)
   
+  
+  object CheckerActor {
+    def props() = Props(new CheckerActor) 
+  }
+  
   class CheckerActor extends Actor {
     def receive = {
       case NextValue(value) =>
-        context.parent ! PartialResult(value, isPrime(value))
+        sender ! PartialResult(value, isPrime(value))
     }
   }
   
@@ -35,11 +36,12 @@ class ActorsPrimesGenerator[NUM](
    */
   class ValuesManagerActor(forActor:ActorRef, precomputedCount:Int=10000, checkerWorkers:Int=4) extends Actor {
     val checkerRouter = context.actorOf(
-        Props(classOf[CheckerActor]).withRouter(SmallestMailboxRouter(checkerWorkers)),
+        CheckerActor.props.withRouter(SmallestMailboxRouter(checkerWorkers)),
         "CheckerActorRouter")
     
     private var currentPrimeNth=primeNth
     private var currentNotPrimeNth=notPrimeNth
+    private var currentValue=startFrom
     private var nextValue=startFrom
 
     // buffered partial results, because we need ordering to compute primes & not primes position 
@@ -51,15 +53,15 @@ class ActorsPrimesGenerator[NUM](
     
     private def processPartialResult(partial:PartialResult) {
       val digitCount = partial.value.toString.size
-      nextValue+=one
+      currentValue+=one
       val nth = if (partial.isPrime) currentPrimeNth else currentNotPrimeNth
       if (partial.isPrime) currentPrimeNth+=one else currentNotPrimeNth+=one
       forActor ! CheckedValue[NUM](partial.value, partial.isPrime, digitCount, nth)
     }
     
     private def flush2order() {
-      while(waitBuffer.size>0 && waitBuffer.contains(nextValue)) {
-        val pr = waitBuffer(nextValue)
+      while(waitBuffer.size>0 && waitBuffer.contains(currentValue)) {
+        val pr = waitBuffer(currentValue)
         processPartialResult(pr)
         waitBuffer = waitBuffer - pr.value
       }
@@ -67,13 +69,13 @@ class ActorsPrimesGenerator[NUM](
     
     private def computeNext() {
       checkerRouter ! NextValue(nextValue)
-      nextValue+=one      
+      nextValue+=one
     }
     
     for { _ <- 1 to precomputedCount } computeNext()
     
     def receive = {
-      case pr:PartialResult if pr.value == nextValue =>
+      case pr:PartialResult if pr.value == currentValue =>
         processPartialResult(pr)
         flush2order()
         computeNext()
@@ -90,17 +92,18 @@ class ActorsPrimesGenerator[NUM](
       case chk:CheckedValue[NUM] => 
         import chk._
         if (chk.isPrime) println(s"$value is the $nth prime number")
-        else println(s"$value is the $nth NOT prime number")
+        //else println(s"$value is the $nth NOT prime number")
     }
   }
+  
+
   
   private val printer = actor("ValuesPrinter") {
     new PrinterActor
   }
   
   private val manager =  actor("ValuesManagerActor") {
-    new ValuesManagerActor(printer, precomputedCount=10000, checkerWorkers=6)
+    new ValuesManagerActor(printer, precomputedCount=1000, checkerWorkers=8)
   }
 
-  
 }
